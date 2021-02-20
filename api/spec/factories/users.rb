63ec6c7e0629406c4726959938e4714daa887f11
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-02-20 12:57:20 +0900

    Add test code for user validation

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "TestName:#{n}"}
-    sequence(:email) { |n| "TestAddress:#{n}@example.com"}
+    sequence(:email) { |n| "TestAddress#{n}@example.com"}
     password  { 'ExamplePassword' }
     password_confirmation  { 'ExamplePassword' }
   end
