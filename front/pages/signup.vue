c17ffb9bd06eb489287607cb43a501611f68530e
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-02-24 01:11:50 +0900

    Add style to signup.vue

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="form-wrapper">
-    <input v-model="name" type="text" placeholder="Your name" />
-    <input v-model="email" type="email" placeholder="Email" />
-    <input v-model="password" type="password" placeholder="Password" />
-    <input
-      v-model="passwordConfirmation"
-      type="password"
-      placeholder="Password Confirm"
-    />
-    <button @click="request_signup">登録</button>
+  <div>
+    <h1 class="title">ユーザ登録</h1>
+    <div class="form-wrapper">
+      <input v-model="name" class="form" type="text" placeholder="Your name" />
+      <input v-model="email" class="form" type="email" placeholder="Email" />
+      <input
+        v-model="password"
+        class="form"
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        v-model="passwordConfirmation"
+        type="password"
+        placeholder="Password Confirm"
+        class="form"
+      />
+      <button @click="request_signup">登録</button>
+    </div>
   </div>
 </template>
 
@@ -43,3 +52,26 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style>
+h1.title {
+  text-align: center;
+}
+
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: auto;
+}
+.form-wrapper > .form {
+  margin: 5px auto;
+  width: 60%;
+  font-size: 24px;
+}
+.form-wrapper > button {
+  margin: 10px auto;
+  width: 40%;
+  font-size: 32px;
+}
+</style>
