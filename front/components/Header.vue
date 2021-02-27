336387c64007d8d551eb7ba2db4887ad110258da
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-02-28 00:22:57 +0900

    Add header components fixed top

---
+++ front/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <button>{{ loginOrLogoutButtonName }}</button>
+    <!-- <button>{{ $auth.$state }}</button> -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      buttonName: 'ログイン',
+    }
+  },
+  computed: {
+    loginOrLogoutButtonName() {
+      return this.$auth.$state.loggedIn ? 'ログアウト' : this.$auth.user
+      //   return this.$store.state.auth.loggedIn
+    },
+  },
+})
+</script>
+
+<style>
+.header {
+  top: 0;
+  width: 100%;
+  background-color: bisque;
+}
+</style>
