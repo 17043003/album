5ef9760aebc543a3886578535bd775c7869f17af
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-03-08 22:08:18 +0900

    Add mypage

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api, { format: 'json' } do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+
+      get 'acount' => 'acount#show'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- front/pages/user/mypage.vue
+++ front/pages/user/mypage.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
     <h1>MyPage</h1>
+    <div v-if="$auth.loggedIn">
+      <img
+        v-if="user.data.image"
+        :src="user.data.image"
+        alt="Your icon image"
+      />
+      <img
+        v-else
+        src="~/assets/default_icon.svg"
+        alt="Your icon image"
+        width="128px"
+        height="128px"
+      />
+      <h2>Your Name : {{ user.data.name }}</h2>
+      <h2>E-mail : {{ user.data.email }}</h2>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $axios }): Promise<void> {
+    return await $axios.get('acount').then((response) => {
+      return { user: response.data }
+    })
+  },
+})
+</script>
